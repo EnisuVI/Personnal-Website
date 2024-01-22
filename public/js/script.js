e81182aea0fdaf76b87b75ca61752cca0f43b3bb
Charly <66905063+EnisuVI@users.noreply.github.com>
2024-01-22 15:09:32 +0100

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    console.log('ready');
+
+    let btn = $('.btn');
+
+    if (localStorage.getItem('theme') == 'night') {
+        document.styleSheets[0].disabled = true;
+        document.styleSheets[1].disabled = false;
+    } else {
+        document.styleSheets[0].disabled = false;
+        document.styleSheets[1].disabled = true;
+    };
+
+    $(btn).click(function() {
+
+
+        if (localStorage.getItem('theme') == 'day') {
+            document.styleSheets[0].disabled = true;
+            document.styleSheets[1].disabled = false;
+            localStorage.setItem('theme', 'night');
+        } else {
+            document.styleSheets[0].disabled = false;
+            document.styleSheets[1].disabled = true;
+            localStorage.setItem('theme', 'day');
+        }
+
+    });
+
+})
