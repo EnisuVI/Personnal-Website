2f8dd1f41770aa7652b6772f80454beaafa9b21f
Charly <cav.photos@orange.fr>
2024-01-23 12:13:41 +0100

    Update galerie

--- public/js/script.js
+++ public/js/script.js
@@ -40,17 +40,4 @@ $(document).ready(function() {
     });
     
 
-    var $grid = $('#galerie').masonry({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        columnWidth: '.grid-sizer',
-      });
-    
-      $grid.imagesLoaded().progress(function() {
-        $grid.masonry();
-      });
-
-
-
 })
-
